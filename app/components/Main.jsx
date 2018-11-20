bebe684ee798169414b937f6157b7808e7b758d2
pentondb <derek.penton@gmail.com>
2018-11-19 21:02:44 -0600

    Add navigation

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,8 +5,12 @@ var Main = (props) => {
     return (
         <div>
             <Nav />
-            <p>Main.jsx rendered</p>
-            {props.children}
+            <div className="row">
+                <div className="columns medium-6 large-4 small-centered">
+                    <p>Main.jsx rendered</p>
+                    {props.children}
+                </div>
+            </div>
         </div>
     );
 }
