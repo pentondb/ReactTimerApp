480f7d95a4bd59f0b11be9ee7fb89c86a548a169
pentondb <derek.penton@gmail.com>
2018-11-20 19:50:45 -0600

    Styled and tested Clock component

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,11 +1,12 @@
 var React = require('react');
+var Clock = require('Clock');
 
 var Countdown = React.createClass({
     render: function () {
         return (
-          <div>
-              Countdown.jsx
-          </div>  
+            <div>
+                <Clock totalSeconds={129} />
+            </div>
         );
     }
 })
